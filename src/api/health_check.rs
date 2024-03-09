f24dc62c0372c49024343b6e6d98c1b06f6fc6a0
alsomeb <alsomeb@gmail.com>
2024-03-10 00:58:43 +0100

    - health_check updated code

--- src/api/health_check.rs
+++ src/api/health_check.rs
@@ -1,7 +1,15 @@
-use actix_web::{get, HttpResponse};
+use actix_web::{get, Responder};
+use actix_web::web::Json;
+
 use crate::api::recipe_api::Response;
 
+/*
+    Handler functions in Actix can return a wide range of objects that implement the Responder trait. This makes it a breeze to return consistent responses from your APIs.
+    Json<T> where T: Serialize (From Serde)
+ */
 #[get("/health")]
-pub async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().json(Response {message: "UP".to_string()})
+pub async fn health_check() -> impl Responder {
+    Json(
+        Response { message: "Server is UP".to_owned() }
+    )
 }
